6187e25e7c2cb704d719549e72285614b9558ccb
Max <1142860498@qq.com>
2024-06-22 21:36:46 +0800

    计算商品总价

--- demo.py
+++ demo.py
@@ -1,2 +1,10 @@
-for it in "python":
+for it in range(1,10):
     print(it)
+
+
+prices = [10,20,30]
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
+
--- main.py
+++ main.py
@@ -26,7 +26,7 @@ while i <= 5:
     i = i + 1
 print("Done")
 
-# 0616  猜谜游戏
+# 0616  猜谜游戏（while循环）
 secret_number = 9
 guess_count = 0
 guess_limit = 3
@@ -41,5 +41,38 @@ else:
 
 # 0616 哥哥是猪猪
 
-for it in "python":
-    print(it)
+# 0616 汽车游戏（while循环）
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car is already started!")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print("Car stopped.")
+    elif command == "help":
+        print("""
+start - to start the car  
+stop - to stop the car
+quite - to quite    
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("I don't understand that...")
+
+
+# 0622 计算商品总价（for循环）
+prices = [10,20,30]
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
