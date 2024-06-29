a7945f8ac0957b9733accb72ab9223f1b5e4c9a2
Max <1142860498@qq.com>
2024-06-29 23:47:29 +0800

    类

--- demo.py
+++ demo.py
@@ -1,12 +1,68 @@
+# 0629 类
+class Point:
+    def move(self):
+        print("move")
 
-# 0629 处理系统错误
-try:
-    age = int(input("Age:"))
-    income =20000
-    risk = income / age
-    print(age)
-except ZeroDivisionError:
-    print("Age cannot be 0.")
-except ValueError:
-    print("Invalid value")
+    def draw(self):
+        print("draw")
+
+
+point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.x)
+point1.draw()
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point = Point(11, 20)
+print(point.x)
+
+
+# person新类型
+class Person:
+    def talk(self):
+        print("talk")
+
+
+john = Person()
+john.talk()
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print("talk")
+
+
+john = Person("John Smith")
+print(john.name)
+john.talk()
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"Hi, I am {self.name}")
+
+
+john = Person("John Smith")
+john.talk()
+
+bob = Person("Bob Smith")
+bob.talk()
 
--- main.py
+++ main.py
@@ -236,3 +236,72 @@ except ZeroDivisionError:
     print("Age cannot be 0.")
 except ValueError:
     print("Invalid value")
+
+# 0629 类
+class Point:
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.x)
+point1.draw()
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point = Point(11, 20)
+print(point.x)
+
+
+# person新类型
+class Person:
+    def talk(self):
+        print("talk")
+
+
+john = Person()
+john.talk()
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print("talk")
+
+
+john = Person("John Smith")
+print(john.name)
+john.talk()
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"Hi, I am {self.name}")
+
+
+john = Person("John Smith")
+john.talk()
+
+bob = Person("Bob Smith")
+bob.talk()
+
